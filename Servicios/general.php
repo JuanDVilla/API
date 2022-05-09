d5b50705e53d085e5272ebdc574adec0d5d2a663
JuanDVilla <juandvillamontoya@gmail.com>
2022-05-09 00:09:48 -0500

    Cambios

--- Servicios/general.php
+++ Servicios/general.php
@@ -2,10 +2,15 @@
 
 date_default_timezone_set('America/Bogota');
 
-define("DBHOST", "localhost:3308");
+/* define("DBHOST", "localhost:3308");
 define("DBNAME", "BolsaEmpleo");
 define("DBUSER", "Bolsa");
-define("DBPASS", "uDx5PlvZAheJHVzt");
+define("DBPASS", "uDx5PlvZAheJHVzt"); */
+
+define("DBHOST", "us-cdbr-east-05.cleardb.net");
+define("DBNAME", "heroku_413bfd11a514660");
+define("DBUSER", "bc414c4ba1cb42");
+define("DBPASS", "e457e6fc");
 
 define("KEY_TOKEN", "XI#f*i2yJ9BnFQRV1uuh");
 
