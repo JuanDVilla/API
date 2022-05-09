b99e0385025ee9fff3d932df4de24d69f94d5981
JuanDVilla <juandvillamontoya@gmail.com>
2022-05-09 01:13:29 -0500

    cambios

--- Servicios/sql.php
+++ Servicios/sql.php
@@ -5,7 +5,7 @@ require_once('general.php');
 $dbo = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 
 
-$sql = "INSERT INTO `usuariosws` (`IDUsuariosWs`, `Nombre`, `Usuario`, `Clave`, `Activo`) VALUES
+echo $sql = "INSERT INTO `usuariosws` (`IDUsuariosWs`, `Nombre`, `Usuario`, `Clave`, `Activo`) VALUES
 (1, 'Bolsa', 'BolsaApi', '5f639f0522f18b1c0217373473fd7011a38743c4', 1);
 COMMIT;";
 $qry_verifica = $dbo->query($sql); 
