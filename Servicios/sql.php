3d724a7eeafe852e725759b1da966a30de7659d5
JuanDVilla <juandvillamontoya@gmail.com>
2022-05-09 01:12:54 -0500

    cambios

--- Servicios/sql.php
+++ Servicios/sql.php
@@ -4,13 +4,10 @@ require_once('general.php');
 
 $dbo = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 
-$qry_verifica = $dbo->query("CREATE TABLE IF NOT EXISTS `usuariosws` (
-    `IDUsuariosWs` int(11) NOT NULL AUTO_INCREMENT,
-    `Nombre` varchar(50) NOT NULL,
-    `Usuario` varchar(50) NOT NULL,
-    `Clave` varchar(100) NOT NULL,
-    `Activo` tinyint(4) NOT NULL,
-    PRIMARY KEY (`IDUsuariosWs`)
-  ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;"); 
+
+$sql = "INSERT INTO `usuariosws` (`IDUsuariosWs`, `Nombre`, `Usuario`, `Clave`, `Activo`) VALUES
+(1, 'Bolsa', 'BolsaApi', '5f639f0522f18b1c0217373473fd7011a38743c4', 1);
+COMMIT;";
+$qry_verifica = $dbo->query($sql); 
   
   exit;
